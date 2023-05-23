8a64987f0a5ca3e6388063c2ba95935d8a5eaed3
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-23 20:44:35 -0300

    for

---
+++ java introduction/javaIntro/src/scanner/examples/ForExample.java
@@ -0,0 +1,20 @@
+package scanner.examples;
+import java.util.Scanner;
+
+public class ForExample {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int num;
+        int j = 0;
+
+        do {
+            System.out.println("");
+            System.out.println("Ingrese un numero");
+            num = read.nextInt();
+            j++;
+            for (int i = 0; i < num; i++) {
+                System.out.print("*");
+            }
+        } while (j < 4);
+    }
+}
