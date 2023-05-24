527f6aad7138d1288dc0683b3b140a0e88fc8562
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-24 20:48:05 -0300

    exercise 13 updated

--- java introduction/javaIntro/src/subprogram/examples/Exercise13.java
+++ java introduction/javaIntro/src/subprogram/examples/Exercise13.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class Exercise13 {
     public static void main(String[] args) {
         String [] equipo = new String[5];
+        Scanner read = new Scanner(System.in);
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println("Ingrese el nombre de su compañero");
+            equipo[i] = read.nextLine();
+        }
+
+        for (int i = 0; i <= 4; i++) {
+            if (i == 0){
+            System.out.print("[ " + equipo[i] + ", ");
+        } else if (i != 4){
+                System.out.print(equipo[i] + ", ");
+            }
+           else {
+                System.out.print(equipo[i] + " ]");
+            }
+        }
     }
 
 }
