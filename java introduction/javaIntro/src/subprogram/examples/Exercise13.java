ddb1709372c7d4e265fb464ff81406c044dbdc51
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-24 20:33:20 -0300

    theory exercise 13

---
+++ java introduction/javaIntro/src/subprogram/examples/Exercise13.java
@@ -0,0 +1,8 @@
+package subprogram.examples;
+import java.util.Scanner;
+public class Exercise13 {
+    public static void main(String[] args) {
+        String [] equipo = new String[5];
+    }
+
+}
