7a427251688b9556aea361532369a897d240ecea
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-24 21:20:54 -0300

    Exercise 2

---
+++ java introduction/javaIntro/src/Exercises1to5/Exercise2.java
@@ -0,0 +1,10 @@
+package Exercises1to5;
+import java.util.Scanner;
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        String name = read.nextLine();
+        System.out.println("Su nombre es "+ name);
+    }
+}
