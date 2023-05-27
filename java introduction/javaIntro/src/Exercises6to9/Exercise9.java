3d90b0cf0ea353131534a542e743994670a353bd
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-27 20:08:29 -0300

    conditional fixed

--- java introduction/javaIntro/src/Exercises6to9/Exercise9.java
+++ java introduction/javaIntro/src/Exercises6to9/Exercise9.java
@@ -5,9 +5,10 @@ public class Exercise9 {
         Scanner read = new Scanner(System.in);
         System.out.println("Ingrese una palabra o frase");
         String word = read.nextLine();
-        String first = word.substring(0).toLowerCase();
+        String first = word.substring(0,1).toLowerCase();
+        System.out.println(first);
 
-        if (first == "a"){
+        if (first.equals("a")){
             System.out.println("La primera letra es una A, CORRECTO");
         } else {
             System.out.println("La primera letra no es una A, INCORRECTO");
