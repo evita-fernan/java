d489df17fe0cdb5967a8fcc2a2e20985f375aef8
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-27 20:15:23 -0300

    Exercise in progress

--- java introduction/javaIntro/src/Loop/Exercise10.java
+++ java introduction/javaIntro/src/Loop/Exercise10.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Exercise10 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese un valor limite");
+        int limit = read.nextInt();
     }
 }
