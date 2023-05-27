57917f21269e05e3b15d517e81538aa072710744
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-27 19:58:36 -0300

    Exercise 9

---
+++ java introduction/javaIntro/src/Exercises6to9/Exercise9.java
@@ -0,0 +1,9 @@
+package Exercises6to9;
+import java.util.Scanner;
+public class Exercise9 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese una palabra o frase");
+
+    }
+}
