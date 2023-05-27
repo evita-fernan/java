aea30f3987499ccc8d4e24853649bc747e3f9d4d
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-26 23:30:14 -0300

    partial conditional

--- java introduction/javaIntro/src/Exercises6to9/Exercise8.java
+++ java introduction/javaIntro/src/Exercises6to9/Exercise8.java
@@ -5,5 +5,11 @@ public class Exercise8 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         System.out.println("Ingrese una palabra o frase");
+        String word = read.nextLine();
+        int qty = word.length();
+
+        if(qty == 8){
+            System.out.println("La cantidad de letras ingresadas es correcta (8)");
+        }
     }
 }
