833199599dc9ca5bd5ba2bb70c24cca99068f7e5
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-29 19:24:23 -0300

    Exercise 11 updated

--- java introduction/javaIntro/src/Exercises10to13/Exercise11.java
+++ java introduction/javaIntro/src/Exercises10to13/Exercise11.java
@@ -1,4 +1,8 @@
 package Exercises10to13;
-
+import java.util.Scanner;
 public class Exercise11 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Seleccione una opción:\n1. Sumar \n2. Restar \n3. Multiplicar \n4. Dividir \n5. Salir \n Elija una opcion:");
+    }
 }
