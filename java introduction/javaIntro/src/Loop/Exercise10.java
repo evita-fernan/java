1da2ec0a14f6d0d813d89ec5b7f98e6aaf282095
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-29 18:52:17 -0300

    Exercise 10 updated

--- java introduction/javaIntro/src/Loop/Exercise10.java
+++ java introduction/javaIntro/src/Loop/Exercise10.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Exercise10 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        System.out.println("Ingrese un valor limite");
+        System.out.println("Ingrese un valor limite positivo");
         int limit = read.nextInt();
+        int sum = 0;
+        int temp = 1;
+        while (sum <= limit) {
+            System.out.println("Ingrese un numero");
+            int num = read.nextInt();
+            sum = sum + num;
+            if (sum > limit) {
+                System.out.println("La suma de los numeros ingresados supera al limite");
+            }
+        }
     }
 }
