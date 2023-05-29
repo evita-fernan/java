ae9bbed2b53e3dab47c669e669dc50eba2f1229a
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-29 19:28:47 -0300

    switch

--- java introduction/javaIntro/src/Exercises10to13/Exercise11.java
+++ java introduction/javaIntro/src/Exercises10to13/Exercise11.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 public class Exercise11 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        System.out.println("Seleccione una opción:\n1. Sumar \n2. Restar \n3. Multiplicar \n4. Dividir \n5. Salir \n Elija una opcion:");
+        System.out.println("Seleccione una opción:\n1. Sumar \n2. Restar \n3. Multiplicar \n4. Dividir \n5. Salir \n Elija un numero:");
+        int option = read.nextInt();
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Ingrese una de las opciones ofrecidas");
+        }
     }
 }
