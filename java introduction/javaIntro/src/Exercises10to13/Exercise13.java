42c9607c31dc6ec762f64aa10d2812267628ca6e
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-30 19:00:40 -0300

    Exercise 13

---
+++ java introduction/javaIntro/src/Exercises10to13/Exercise13.java
@@ -0,0 +1,7 @@
+package Exercises10to13;
+import java.util.Scanner;
+public class Exercise13 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+    }
+}
