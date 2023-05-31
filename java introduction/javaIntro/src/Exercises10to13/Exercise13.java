bf1d9e647e27112de7f02856787a12b2c9b9ec90
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-30 21:30:20 -0300

    changing

--- java introduction/javaIntro/src/Exercises10to13/Exercise13.java
+++ java introduction/javaIntro/src/Exercises10to13/Exercise13.java
@@ -3,7 +3,26 @@ import java.util.Scanner;
 public class Exercise13 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
+        int counter = 0;
+
+        System.out.println("");
         System.out.println("Ingrese un numero entero positivo");
-        int num = read.nextInt();
+        counter = read.nextInt();
+
+        for (int i = 0; i < counter; i++) {
+            for (int j = 0; j < counter; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+          if(i==0){
+                System.out.println("*");
+            } else if(i==(counter-1)){
+                System.out.println("*");
+            }
+            else {
+                System.out.print(" ");
+            }
+        }
+
     }
 }
