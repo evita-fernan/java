34146a18de84589e6b4b62ef7a92cabd2d732c43
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-30 21:14:07 -0300

    updated

--- java introduction/javaIntro/src/Exercises10to13/Exercise13.java
+++ java introduction/javaIntro/src/Exercises10to13/Exercise13.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Exercise13 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero positivo");
+        int num = read.nextInt();
     }
 }
