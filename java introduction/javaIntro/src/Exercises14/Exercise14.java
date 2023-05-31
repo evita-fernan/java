88b937dc6ea36583cdb995103b1e363438b7eaf5
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-31 19:28:35 -0300

    updated

--- java introduction/javaIntro/src/Exercises14/Exercise14.java
+++ java introduction/javaIntro/src/Exercises14/Exercise14.java
@@ -6,11 +6,16 @@ public class Exercise14 {
         Scanner read = new Scanner (System.in);
         System.out.println("Ingrese la cantidad de euros a convertir");
         double euros = read.nextDouble();
-        System.out.println("Ingrese la moneda a convertir");
-        String type = read.nextLine();
+        System.out.println("Seleccione la moneda a convertir (escoja el numero):\n1. Dolares \n2. Yenes \n3. Libras");
+        int type = read.nextInt();
     }
 
     public static void currencyConvertion (double euros, String type){
-        
+        double result = 0;
+        String dolars;
+        String yenes;
+        String pounds;
+
+
     }
 }
