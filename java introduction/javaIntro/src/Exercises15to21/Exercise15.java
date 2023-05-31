9bf13ce0f5fa377a2d1a01491f871162b83d66cb
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-31 19:52:39 -0300

    Exercise 15

---
+++ java introduction/javaIntro/src/Exercises15to21/Exercise15.java
@@ -0,0 +1,11 @@
+package Exercises15to21;
+import java.util.Scanner;
+
+public class Exercise15 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int [] vector = new int[100];
+        int j = 1;
+
+    }
+}
