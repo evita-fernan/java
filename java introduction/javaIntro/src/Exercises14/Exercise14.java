5430168a5695c4ce9cf120bfc1def0e8952c08b6
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-31 19:21:09 -0300

    Exercise 14

---
+++ java introduction/javaIntro/src/Exercises14/Exercise14.java
@@ -0,0 +1,16 @@
+package Exercises14;
+import java.util.Scanner;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner (System.in);
+        System.out.println("Ingrese la cantidad de euros a convertir");
+        double euros = read.nextDouble();
+        System.out.println("Ingrese la moneda a convertir");
+        String type = read.nextLine();
+    }
+
+    public static void currencyConvertion (double euros, String type){
+        
+    }
+}
