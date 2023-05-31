7ebad6e2cc1cac0ef419cdb21735cc230d5f6268
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-05-31 19:53:22 -0300

    finished

--- java introduction/javaIntro/src/Exercises15to21/Exercise15.java
+++ java introduction/javaIntro/src/Exercises15to21/Exercise15.java
@@ -6,6 +6,16 @@ public class Exercise15 {
         Scanner read = new Scanner(System.in);
         int [] vector = new int[100];
         int j = 1;
-
+        for(int i=0; i<vector.length; i++){
+            vector[i] = j;
+            j++;
+            if(i==0){
+                System.out.print("[ " + vector[i]);
+            }
+            else if(i==99){
+                System.out.print(vector[i] + " ]");
+            }
+            else System.out.print(" " + vector[i] + " ");
+        }
     }
 }
