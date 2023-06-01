6002c7ee4c5faf16365d4f6a03e13193b530d001
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-06-01 19:49:18 -0300

    updating

--- java introduction/javaIntro/src/Exercises15to21/Exercise17.java
+++ java introduction/javaIntro/src/Exercises15to21/Exercise17.java
@@ -2,6 +2,10 @@ package Exercises15to21;
 import java.util.Scanner;
 public class Exercise17 {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero positivo para dimensionar un vector");
+        int n = read.nextInt();
+        int [] vector = new int[n];
         
     }
 }
