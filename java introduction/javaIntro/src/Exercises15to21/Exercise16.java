a79c61cb0fd562388a3821ed416864286c2ec370
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-06-01 19:15:38 -0300

    updating

---
+++ java introduction/javaIntro/src/Exercises15to21/Exercise16.java
@@ -0,0 +1,24 @@
+package Exercises15to21;
+import java.util.Scanner;
+
+
+/*
+Realizar un algoritmo que rellene un vector de tamaño N con valores aleatorios y le pida
+al usuario un numero a buscar en el vector. El programa mostrará donde se encuentra el
+numero y si se encuentra repetido
+* */
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero positivo para dimensionar el vector");
+        int n = read.nextInt();
+        int [] vector = new int[n];
+
+        System.out.println("Escoja un numero");
+        int num = read.nextInt();
+    }
+    public static void search (int n, int vector[]){
+
+    }
+}
