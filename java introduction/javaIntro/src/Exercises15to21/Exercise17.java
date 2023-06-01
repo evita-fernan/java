9a42d0eac33574f4dd04236b1f99e371950ff9a2
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-06-01 20:14:36 -0300

    updating

--- java introduction/javaIntro/src/Exercises15to21/Exercise17.java
+++ java introduction/javaIntro/src/Exercises15to21/Exercise17.java
@@ -7,5 +7,31 @@ public class Exercise17 {
         int n = read.nextInt();
         int [] vector = new int[n];
 
+        completeVector(n, vector);
+        countDigits(n, vector);
+    }
+
+    public static void completeVector(int n, int [] vector){
+        int min = 0;
+        int max = 50;
+        for (int i = 0; i < n; i++) {
+            //In the following formula we create a program that generates random numbers between a minimum and a maximum value
+            vector[i] = (int) ((Math.random() * ((max - min) + 1)) + min);
+            System.out.println(vector[i]);
+        }
+        System.out.println("Vector de tamanio " + n + " completado con valores aleatorios entre "  + min + " y " + max);
+    }
+
+    public static void countDigits(int n, int[] vector) {
+        int counter = 0;
+        for (int i=0; i<n; i++){
+            if(vector[i] <= 10){
+                counter++;
+            }
+            System.out.println("La cantidad de numeros con digito 1 son " + counter);
+            if(vector[i]> 10){
+
+            }
+        }
     }
 }
