c2bdb49786b9fe9cc7b030f79d8ca9d73da999d3
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-06-02 14:54:37 -0300

    updating

---
+++ java introduction/javaIntro/src/Exercises15to21/Exercise18.java
@@ -0,0 +1,9 @@
+package Exercises15to21;
+import java.util.Scanner;
+public class Exercise18 {
+    public static void main(String[] args) {
+        int n = 4;
+        int [] vector = new int[n];
+        
+    }
+}
