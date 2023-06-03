d3edd794609775eb45f2f747f2b98673b3cb8b51
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-06-02 21:14:17 -0300

    updating

--- java introduction/javaIntro/src/Exercises15to21/Exercise18.java
+++ java introduction/javaIntro/src/Exercises15to21/Exercise18.java
@@ -1,9 +1,26 @@
 package Exercises15to21;
-import java.util.Scanner;
+
 public class Exercise18 {
     public static void main(String[] args) {
         int n = 4;
-        int [] vector = new int[n];
-        
+        int[][] matrix = new int[n][n];
+        int[][] transpMatrix = new int[n][n];
+        completeRandomMatrix(n, matrix);
+        System.out.println("La matriz aleatoria es: ");
+
+
     }
+    //funcion que completa un matriz con numeros aleatorios
+    public static int[][] completeRandomMatrix(int n, int[][] matrix) {
+        int min = 0;
+        int max = 50;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //In the following formula we create a program that generates random numbers between a minimum and a maximum value
+                matrix[i][j] = (int) ((Math.random() * ((max - min) + 1)) + min);
+            }
+        }
+        return matrix;
+    }
+
 }
