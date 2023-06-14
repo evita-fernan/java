20f569d63c74474c8acd8c6312ed9eb0a1cc6830
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-06-14 20:21:07 -0300

    services

---
+++ poo/MascotaAPP/src/mascotaapp/services/MascotaService.java
@@ -0,0 +1,13 @@
+package mascotaapp.services;
+
+import mascotaapp.entidades.Mascota;
+
+import java.util.Scanner;
+
+public class MascotaService {
+
+    private Scanner read = new Scanner(System.in);
+    public Mascota crearMascota(){
+
+    }
+}
