6673811f835356786c705d4e79026de034733223
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2023-06-20 15:01:33 -0300

    updating

--- oop-exercises/exercise1/src/entidades/Libro.java
+++ oop-exercises/exercise1/src/entidades/Libro.java
@@ -1,10 +1,19 @@
 package entidades;
 
 public class Libro {
-
     public int ISBN;
     public String title;
     public String author;
     public int pageNumber;
 
+    //constructor
+    public Libro (){
+    }
+
+    public Libro(int ISBN, String title, String author, int pageNumber) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.pageNumber = pageNumber;
+    }
 }
